6e42787d35f4aaf4a9798b85e10cc803f6323d07
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-26 15:51:45 +0800

    refactor: delete imported auth service and auth provider at app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { SignupModule } from './signup/signup.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, SignupModule],
+  imports: [UserModule, SignupModule, AuthModule],
 })
 export class AppModule {}
