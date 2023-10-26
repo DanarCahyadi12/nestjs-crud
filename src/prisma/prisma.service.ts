85d1853b0c164f353bd5e3584bb0392996e0c10b
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-26 13:53:29 +0800

    config a database connection and define models

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {}
+}
