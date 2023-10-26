3e69b0e93c913c3fbb562e85e43b1337b7c3d37e
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-26 13:54:07 +0800

    fix: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { SignupModule } from './signup/signup.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, SignupModule],
 })
 export class AppModule {}
