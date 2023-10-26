8817109521aca50b712c236ba4dba20a6c4ce3fc
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-26 17:40:20 +0800

    chore: adding decorator

---
+++ src/user/decorators/user.decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext } from '@nestjs/common';
+
+export const User = (context: ExecutionContext, ...datas) => {
+  const req = context.switchToHttp().getRequest();
+  const user = req.UserController;
+  if (datas.length === 0) return user;
+  const result: object = {};
+  datas.forEach((key) => {
+    result[key] = user[key];
+  });
+
+  return result;
+};
