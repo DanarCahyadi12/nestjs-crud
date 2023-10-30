fce8868c2c59390f32f0c5df3d9bc8ee30fb0abe
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-30 16:26:55 +0800

    chore: change result based on parameters

--- src/user/decorators/user.decorator.ts
+++ src/user/decorators/user.decorator.ts
@@ -1,14 +1,17 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const User = createParamDecorator(
-  (datas: string[] | undefined, ctx: ExecutionContext) => {
+  (datas: string[] | string | undefined, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     const user = req.user;
     if (!datas) return user;
     const result: object = {};
-    datas.forEach((key) => {
-      result[key] = user[key];
-    });
-    return result;
+    if (typeof datas === 'object') {
+      datas.forEach((key) => {
+        result[key] = user[key];
+      });
+      return result;
+    }
+    return user?.[datas];
   },
 );
