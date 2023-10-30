32907dcb93827fc28af3f9d3f6082d620dabf724
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-30 15:46:44 +0800

    fix: return all data user when not passing a parameter

--- src/user/decorators/user.decorator.ts
+++ src/user/decorators/user.decorator.ts
@@ -1,10 +1,10 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const User = createParamDecorator(
-  (datas: string[], ctx: ExecutionContext) => {
+  (datas: string[] | undefined, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     const user = req.user;
-    if (datas.length === 0) return user;
+    if (!datas) return user;
     const result: object = {};
     datas.forEach((key) => {
       result[key] = user[key];
