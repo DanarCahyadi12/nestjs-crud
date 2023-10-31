8f6aec1b00472c08ffbe1a453eb111396d9e68e6
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-31 16:38:58 +0800

    refactor: using port from env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
