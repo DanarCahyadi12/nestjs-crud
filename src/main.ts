5500b5831d91a6021d7aa270909f9af979e0ef47
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-01 11:59:42 +0800

    chore: adding ip address

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(process.env.SERVER_PORT);
+  await app.listen(process.env.SERVER_PORT, process.env.IP_ADDRESS);
 }
 bootstrap();
